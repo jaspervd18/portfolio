54e59778e947e5a22bfda7c37728f109a50af4a2
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-04-02 21:39:01 +0200

    remove background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ export default {
       colors: {
         primary: "#050816",
         secondary: "#aaa6c3",
-        tertiary: "#151030",
+        tertiary: "#101938",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
@@ -17,9 +17,6 @@ export default {
       },
       screens: {
         xs: "450px",
-      },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
       },
     },
   },
